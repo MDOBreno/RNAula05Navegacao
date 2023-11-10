70a87413146fa79e4a23907ed2ef711431f317c0
Breno Medeiros De Oliveira <>
2023-11-10 10:46:50 -0300

    - Secao06Aula88 - Navegacao Stack 02

--- src/views/TelaA.js
+++ src/views/TelaA.js
@@ -2,8 +2,14 @@ import React from 'react';
 //import {} from 'react-native';
 import TextoCentral from '../components/TextoCentral';
 
-export default props => (
-    <TextoCentral corFundo='#e53935'>
-        Tela A
-    </TextoCentral>
-)
+export default props => {
+    //console.warn(props.navigation != null); //Exibe: true
+    // console.warn(props.navigation.navigate != null); //Exibe: true
+    props.navigation.navigate("TelaB");
+
+    return (
+        <TextoCentral corFundo='#e53935'>
+            Tela A
+        </TextoCentral>
+    )
+}
