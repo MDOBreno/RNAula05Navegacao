53ab582f1f37efcdda914fe0b46a36588c471f6a
Breno Medeiros De Oliveira <>
2023-11-10 07:45:18 -0300

    - Secao06Aula85 - Preparando App 01

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Safe, SafeAreaView, Text } from 'react-native'
+import TextoCentral from "./components/TextoCentral";
+
+export default props => (
+    <SafeAreaView style={{flex:1}}>
+        <TextoCentral>
+            App
+        </TextoCentral>
+    </SafeAreaView>
+)
